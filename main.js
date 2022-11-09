c666945cb8f1135eeb3b5cbea8b49d103060efbb
Nicolas Martinez <martineznicolas551@gmail.com>
2022-11-09 20:41:34 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+//Ejercicio 1
+
+var medioDeTransporte = parseInt(prompt('¿Cuantos metros necesitas recorrer?'));
+
+if (medioDeTransporte > 100000) {
+  document.write('Deberias ir en avion');
+} else if (medioDeTransporte > 30000) {
+  document.write('Deberias ir en auto');
+} else if (medioDeTransporte > 10000) {
+  document.write('Deberias ir en colectivo');
+} else if (medioDeTransporte > 1000) {
+  document.write('Deberias ir en bicicleta');
+} else if (medioDeTransporte > 0) {
+  document.write('Deberias ir a pie');
+}
+
+//Ejercicio 2
+
+let mayorNumero = 0;
+let numeros = [2255, 1, 66, 12, 77, 233, 45, 10];
+
+for (let i = 0; i < numeros.length; i++) {
+  if (numeros[i] > mayorNumero) {
+    mayorNumero = numeros[i];
+  }
+}
+
+document.write(`<br> El numero mayor del array es: ` + mayorNumero);
